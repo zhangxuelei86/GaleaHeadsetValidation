1dea0083d1053504c62cb0fd680b44b803b83526
Ziheng(Leo) Li <s-vector.lee@hotmail.com>
2021-10-20 11:54:57 -0400

    update MVT to include RestState

--- MechanicalVariationTest.py
+++ MechanicalVariationTest.py
@@ -5,6 +5,7 @@ import os
 from postprocess.P300PostProcess import post_process_p300
 
 # user parameters ######################################################
+from postprocess.RestStatePostProcess import post_process_rest_state
 from postprocess.SSVEPPostProcess import post_process_ssvep
 
 notes = 'MVT Spring Partial 10/15/20'
@@ -20,6 +21,16 @@ p300_tmax = 0.5
 ssvep_tmin = -0.5
 ssvep_tmax = 4.0
 
+tmin_focus = 0.2
+tmax_focus = 9.8
+tmin_rest = 0.2
+tmax_rest = 29.8
+
+timelocking_dict = {
+    'Focus': [1, 9.8],
+    'Rest': [1, 29.8]
+}
+
 resample_f = 50
 
 df = pd.read_csv(path_to_notes)
@@ -30,10 +41,16 @@ mvts = pd.unique(df['MVT'])  # find the MVT
 
 for mvt in mvts:
     # process p300
-    P300_rows = df.loc[(df['MVT'] == mvt) & (df['Experiment'] == "P300")]
-    p300_data_path_list = [os.path.join(data_root, x) for x in P300_rows['FileName'].values]
-    post_process_p300(p300_data_path_list, 'Galea', 'x', notes, is_plotting, is_exporting_samples, p300_tmin, p300_tmax, resample_f)
+    # P300_rows = df.loc[(df['MVT'] == mvt) & (df['Experiment'] == "P300")]
+    # p300_data_path_list = [os.path.join(data_root, x) for x in P300_rows['FileName'].values]
+    # post_process_p300(p300_data_path_list, 'Galea', 'x', notes, is_plotting, is_exporting_samples, p300_tmin, p300_tmax, resample_f)
+    #
+    # # process SSVEP
+    # SSVEP_rows = df.loc[(df['MVT'] == mvt) & (df['Experiment'] == "SSVEP")]
+    # SSVEP_data_path_list = [os.path.join(data_root, x) for x in SSVEP_rows['FileName'].values]
+    # post_process_ssvep(SSVEP_data_path_list, 'Galea', 'x', notes, ssvep_tmin, ssvep_tmax)
 
-    SSVEP_rows = df.loc[(df['MVT'] == mvt) & (df['Experiment'] == "SSVEP")]
-    SSVEP_data_path_list = [os.path.join(data_root, x) for x in SSVEP_rows['FileName'].values]
-    post_process_ssvep(SSVEP_data_path_list, 'Galea', 'x', notes, ssvep_tmin, ssvep_tmax)
+    # process RestState
+    RestState_rows = df.loc[(df['MVT'] == mvt) & (df['Experiment'] == "RestState")]
+    RestState_data_path_list = [os.path.join(data_root, x) for x in RestState_rows['FileName'].values]
+    post_process_rest_state({'Galea': RestState_data_path_list}, timelocking_dict, 'x', notes)
